edc0ad4037482c5b8e65d74311f08e13beb5ef63
helinz1 <130932031+helinz1@users.noreply.github.com>
2023-05-29 21:23:55 +0300

    init

---
+++ magic.js
@@ -0,0 +1,23 @@
+const phraseElement = document.getElementById('phrase');
+
+// Array of phrases
+const phrases = 
+   [ 
+      "Yes",
+      "No",
+      "I don't think so",
+      "Definitely",
+      "You should ",
+      "You shouldn't"
+   ];
+   style.textAlign = "center";
+
+// Function to be called on button click
+function getPhrase() {
+   // Get a random number from 1 to the length of phrases array length
+   let randomPhraseIndex = Math.floor(Math.random() * phrases.length);
+   console.log(randomPhraseIndex);
+
+   // Set the phrase element text to the string found at phrases
+   phraseElement.innerText = phrases[randomPhraseIndex];
+}
